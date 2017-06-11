477687c8c815f150eaa0bfc82ea41834178392a0
Guaiamum <cainabrazil@gmail.com>
2017-06-11 17:50:57 -0300

    first commit - spinning cube

---
+++ Assets/Scripts/RotateAround.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour {
+
+	private Vector3 point, axis;
+	private float angle;
+
+	// Use this for initialization
+	void Start () {
+		point = Vector3.zero;
+		axis = Vector3.up;
+		angle = 200 * Time.deltaTime;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		this.transform.RotateAround (point, axis, angle);	
+	}
+}
